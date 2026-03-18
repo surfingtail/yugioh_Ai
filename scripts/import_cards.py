f8450cac7f5a2e834ced4d255e9f8ec84c194f97
surfingtail <familyhuryoon@gmail.com>
2026-03-18 20:41:16 +0900

    tested import_cards.py

--- scripts/import_cards.py
+++ scripts/import_cards.py
@@ -1,12 +1,5 @@
 from src.db.import_cards import import_cards
-from src.db.connection import get_db_connection, close_db_connection
 
 
 if __name__ == "__main__":
     import_cards()
-
-    conn = get_db_connection()
-    cursor = conn.execute("SELECT COUNT(*) FROM cards")
-    print(cursor.fetchone())
-
-    close_db_connection(conn)
